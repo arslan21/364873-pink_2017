39979148e181c07c0a8fb92cc469bbc667da833d
Arslan <rauros@list.ru>
2017-10-21 00:48:13 +0300

    changed width 100% to min-width

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//https://up.htmlacademy.ru/adaptive/12/demos/343#31
+//скрипт открытия закрыия меню
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
